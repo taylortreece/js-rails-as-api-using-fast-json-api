1ee075f77a567807e94ed66e6794a69565c50495
taylortreece <tayloratreece@gmail.com>
2021-07-30 16:37:08 -0500

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,8 +1,8 @@
 class SightingsController < ApplicationController
-  def index
-    sighting = Sighting.all
-    render json: SightingSerializer.new(sighting)
-  end
+  # def index
+  #   sighting = Sighting.all
+  #   render json: SightingSerializer.new(sighting)
+  # end
 
   def show
     sighting = Sighting.find(params[:id])
--- app/serializers/sighting_serializer.rb
+++ app/serializers/sighting_serializer.rb
@@ -1,4 +1,4 @@
 class SightingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes
 end
